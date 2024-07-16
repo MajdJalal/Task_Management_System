aa2d76e1f00d42353e6dbe889cf0829ba0c4274a
MajdJalal <97984747+MajdJalal@users.noreply.github.com>
2024-07-16 21:02:15 +0300

    Update TaskProcessor.java
    added javadoc to methods

--- taskManagement/src/main/java/org/digitinary/taskManagement/entities/TaskProcessor.java
+++ taskManagement/src/main/java/org/digitinary/taskManagement/entities/TaskProcessor.java
@@ -7,10 +7,17 @@ public class TaskProcessor {
 
     ExecutorService executorService;
 
+    /**
+     * this method creates a pool of threads of size 10
+     */
     public TaskProcessor() {
         this.executorService =  Executors.newFixedThreadPool(10);
     }
 
+    /**
+     * this method execute a runnable task's run method
+     * @param task
+     */
     public void processTask(Task task) {
         executorService.execute(task);
     }
